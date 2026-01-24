a45c0779d41419069e136f6be84df3508c6ede35
KacperWojciechowski <k.woj.coding@gmail.com>
2026-01-24 08:37:37 +0000

    Removed redundant .env sourcing in run.sh

--- docker/run.sh
+++ docker/run.sh
@@ -10,17 +10,11 @@ export MSYS_NO_PATHCONV=1
 SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 source "$SCRIPT_DIR/Resources/logging.sh"
 
-set -a
-source <(grep -v '^\s*#' $SCRIPT_DIR/.env | grep -v '^\s*$')
-set +a
-
 OS_TYPE="$(uname -s)"
 
 DOCKER_RUN_ARGS=(
 	-it
 	--rm
-	-e GIT_USER_NAME=$GIT_USER_NAME
-	-e GIT_USER_EMAIL=$GIT_USER_EMAIL
 	-v "${VOLUME_NAME}:${CONTAINER_WORKDIR}"
 	-w "${CONTAINER_WORKDIR}"
 	-v "${HOME}/.ssh/id_ed25519:/home/Dev/.ssh/id_ed25519:ro"
